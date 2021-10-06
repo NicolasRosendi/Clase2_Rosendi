2d38860e01c4fb5a179f523478369780d9860dd3
NicolasRosendi <92051703+NicolasRosendi@users.noreply.github.com>
2021-10-06 18:27:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let resultado = prompt("¿Cuál es el resultado de 6:2(2+1)");
 
-if (resultado == "1"){
+if (resultado == "9"){
     alert("Su respuesta es correcta");
 } else{
     alert("intente de nuevo");
